50a60e3dd2e4c6c9dc2c804ba2e50ccd3b87359c
Alejandro-Huaman <U201910144@upc.edu.pe>
2022-10-23 03:36:30 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MaterialModule } from './material-module';
+
+@NgModule({
+  declarations: [	
+    AppComponent,
+    LoginComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
