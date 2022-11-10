f08274a41be8d68799b241c758bd64483151c725
Alejandro-Huaman <U201910144@upc.edu.pe>
2022-11-10 18:39:47 -0500

    finished inside sales

--- src/app/pages/sales/inside-sale/inside-sale.component.ts
+++ src/app/pages/sales/inside-sale/inside-sale.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inside-sale.component.css']
 })
 export class InsideSaleComponent implements OnInit {
-
+  hours:string[] = []
+  templatehour:string = ":00"
   constructor() { }
 
   ngOnInit() {
+    for(let i=1;i<25;i++){
+      this.templatehour = String(i) + this.templatehour;        
+      this.hours.push(this.templatehour)
+      this.templatehour = ":00"    
+    }
   }
+
 
 }
