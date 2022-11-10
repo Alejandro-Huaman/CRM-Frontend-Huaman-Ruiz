b53c5d5f4cbd0ec3c3ae8e6ae12756f2b1c9da49
Alejandro-Huaman <U201910144@upc.edu.pe>
2022-11-10 09:59:13 -0500

    create sales and customers design

--- src/app/pages/sales/sales.component.ts
+++ src/app/pages/sales/sales.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales.component.css']
 })
 export class SalesComponent implements OnInit {
-
+  list:number[] = [1,2,3,4,5]
+  
   constructor() { }
 
   ngOnInit() {
