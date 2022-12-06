d3dbc91e0e134ba9445edf252b34b92f60435f1f
Alejandro-Huaman <U201910144@upc.edu.pe>
2022-12-05 22:06:09 -0500

    advance of show date with date format

--- src/app/pages/activities/activities.component.ts
+++ src/app/pages/activities/activities.component.ts
@@ -1,3 +1,4 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
@@ -12,7 +13,9 @@ import { TaskService } from 'src/services/task/task.service';
 export class ActivitiesComponent implements OnInit {
   displayedColumns: string[] = ['title', 'date', 'description', 'sale','customer'];
   dataSource!:MatTableDataSource<any>;
-  
+  startdateformatselected!:any
+  pipedate:DatePipe = new DatePipe("en-US")
+
   @ViewChild(MatPaginator, {static: true}) paginator!:MatPaginator 
 
   constructor(public dialog:MatDialog, private taskService:TaskService) { 
@@ -28,6 +31,18 @@ export class ActivitiesComponent implements OnInit {
       this.dataSource.data = response.content
       this.dataSource.paginator = this.paginator
       console.log(this.dataSource.data)
+
+      for(var onetask of this.dataSource.data){
+        console.log(onetask)
+        console.log(onetask.date)
+        this.startdateformatselected = this.pipedate.transform(onetask.date, 'dd/MM/yyyy')
+        
+        console.log(typeof this.startdateformatselected)
+        console.log(this.startdateformatselected)
+
+        onetask.date = this.startdateformatselected
+      }
+
     });
   }
 }
