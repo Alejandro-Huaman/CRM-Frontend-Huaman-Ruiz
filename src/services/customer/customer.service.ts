014dfc5a589b5f830f1a5fca75d14bef5a687c35
Alejandro-Huaman <U201910144@upc.edu.pe>
2023-01-13 06:36:24 -0500

    hotfix on customer service url path

--- src/services/customer/customer.service.ts
+++ src/services/customer/customer.service.ts
@@ -8,7 +8,7 @@ import { Customer } from 'src/app/models/customer';
 })
 export class CustomerService {
 
-  basePath = 'https://crm-backend-huaman-ruiz-production.up.railway.app/api/v1/users';
+  basePath = 'https://crm-backend-huaman-ruiz-production.up.railway.app/api/v1/customers';
   //basePath = 'http://localhost:8080/api/v1/customers';
   
   
